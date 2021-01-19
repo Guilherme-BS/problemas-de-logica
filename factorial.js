b49c6ce18567c6097ffed8f1611308ae9d66ce4a
Guilherme-BS <guilhermebarbo17@hotmail.com>
2021-01-19 19:24:24 -0300

    insercao do problema factorial

---
+++ factorial.js
@@ -0,0 +1,19 @@
+const  testCase = [5, 3 ,2]
+
+function calculaFactorial (numbers){
+    totalDoFatorial = 1
+    for(let i = 1; i <= numbers ; i++){
+        totalDoFatorial *= i       
+    }
+    return totalDoFatorial
+}
+
+function somadeFactorial (factorial){
+    let totaldosFactorials = 0
+    for (let i = 0; i < factorial.length; i ++){
+        totaldosFactorials += calculaFactorial(factorial[i])
+        
+    }
+    return totaldosFactorials
+}
+console.log(somadeFactorial(testCase)) 
